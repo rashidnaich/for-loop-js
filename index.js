2e1dc216462050e75bee71e5c2b75db82bc48f37
Muhammad Rashid <rashidnaich88@gmail.com>
2025-01-16 23:49:12 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var cities = [
+    "Karachi", "Lahore", "FaisAlaBad", "Rawalpindi", "Multan",
+    "Hyderabad", "Gujranwala", "Peshawar", "Quetta",
+    "Islamaad", "Sialkot", "Bahawalpur", "Sukkur",
+    "Larkana", "Mingora", "Sahiwal", "Dera Ghazi Khan",
+    "Mirpur Khas", "Chiniot", "Okara" 
+    ];
+  
+  
+    for (var i = 0; i <= cities.length; ++i ){
+      if ( city === 'Islamabad'){
+        console.log("Islamabad is the cleanest city of [pakistan")
+        console.log(i)
+        break; 
+      }
+    }
+  
